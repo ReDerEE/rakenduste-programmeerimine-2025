733f44435bc8f2effd537bf3f213d2cc9a10c214
ReDerEE <108543563+ReDerEE@users.noreply.github.com>
2025-09-14 17:51:30 +0300

    build: created gh-pages build

--- 2025-09-11/my-app/src/componenets/Dice.tsx
+++ 2025-09-11/my-app/src/componenets/Dice.tsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react'
-
-
+import { useState } from "react"
 
 function Dice() {
-    const [dieNum, setDie] = useState(1)
-    function rollDice(){
-        setDie(Math.ceil(Math.random()*6))
-    }
-
+  const [dieNum, setDie] = useState(1)
+  function rollDice() {
+    setDie(Math.ceil(Math.random() * 6))
+  }
 
   return (
     <div>
-    <button onClick={rollDice}>Roll the dice</button>
-    <br />
-    {dieNum}
+      <button onClick={rollDice}>Roll the dice</button>
+      <br />
+      {dieNum}
     </div>
   )
 }
