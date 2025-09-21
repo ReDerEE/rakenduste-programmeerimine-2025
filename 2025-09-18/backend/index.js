dd45b86d6b8070f189ff6f73ce2befb4cf4c5f46
ReDerEE <108543563+ReDerEE@users.noreply.github.com>
2025-09-21 16:43:22 +0300

    feat: add CRUD endpoints to backend

--- 2025-09-18/backend/index.js
+++ 2025-09-18/backend/index.js
@@ -3,7 +3,37 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello gayboiiii!");
+  res.send("Somebody requested something");
+});
+
+app.post("/", (req, res) => {
+  res.send("Somebody sent something");
+});
+
+app.put("/", (req, res) => {
+  res.send("Somebody updated something");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Somebody deleted something");
+});
+
+app.get("/manufacturer/:manufacturerId/product/:productId", (req, res) => {
+  res.send(
+    "Requested manufacturer " +
+      req.params.manufacturerId +
+      ", product " +
+      req.params.productId
+  );
+});
+
+app.get("/events/:dateStart-:dateEnd", (req, res) => {
+  res.send(
+    "Requested events from " +
+      req.params.dateStart +
+      " to " +
+      req.params.dateEnd
+  );
 });
 
 app.listen(port, () => {
