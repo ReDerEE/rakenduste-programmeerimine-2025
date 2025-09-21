af82b2655d20bb9e32f28ab59298470354ff7e69
ReDerEE <108543563+ReDerEE@users.noreply.github.com>
2025-09-21 16:25:00 +0300

    feat: add useLocalStorage hook with usage

--- 2025-09-18/my-app/src/components/MuiButton.tsx
+++ 2025-09-18/my-app/src/components/MuiButton.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@mui/material"
+// import { useState } from "react"
+import useLocalStorage from "../functions/useLocalStorage"
 // import "../style/muiButton.css"
 
 function MuiButton() {
+  const [counter, setCounter] = useLocalStorage<number>("counter", 0)
   return (
     <div>
       <div id="muiButton">
-        <Button variant="contained">sii olema njupp</Button>
+        <Button
+          variant="contained"
+          onClick={() => setCounter(counter+1)}
+        >
+          sii olema njupp
+        </Button>
+        <br />
+        {counter}
       </div>
     </div>
   )
